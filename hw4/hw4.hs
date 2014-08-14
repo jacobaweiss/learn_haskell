759284c2d6a2e097960211f3c3b8bfef453fffbc
Jack Weiss <jacobaweiss@gmail.com>
2014-08-13 19:35:14 -0700

    Solve problem 2 of hw4

--- hw4/hw4.hs
+++ hw4/hw4.hs
@@ -2,5 +2,9 @@ module HW4 where
 
 import Data.List
 
-fun1' :: [Int] -> Int
+fun1' :: [Integer] -> Integer
 fun1' = product . map (-2+) . filter (even)
+
+fun2' :: Integer -> Integer
+fun2' n = sum . filter even $ takeWhile (>1) $ iterate nextStep n
+          where nextStep n = if even n then n `div` 2 else 3 * n + 1
